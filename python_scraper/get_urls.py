cdc2008748a71706f1c946aa74e3bb527348e928
davisansons <davis.ansons@outlook.com>
2022-07-12 18:40:28 +0100

    created function to scrape data into dictionary format

--- python_scraper/get_urls.py
+++ python_scraper/get_urls.py
@@ -1,14 +1,55 @@
-from bs4 import BeautifulSoup
+from time import sleep
 import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
-def get_urls_function(url):
-    request_url = requests.get(url)
-    soup = BeautifulSoup(request_url.text, "html.parser")
-    url_list = []
-    results = soup.find(id='maincontent')
-    results_elements = results.find_all("a", class_="product")
-    for element in results_elements:
-        url_list.append(element["href"])
-    print(url_list)
-    return url_list
+def get_child_url(url):
+    
+    child_urls = []
 
+    source_data = requests.get(url)
+    soup = BeautifulSoup(source_data.text, 'html.parser')
+    content = soup.find(id='maincontent')
+    content_elements = content.find_all('a', class_='product')
+    for element in content_elements:
+        child_urls.append(element['href'])
+    return child_urls
+    
+
+
+def get_all_data(child_urls):
+    dict_list = []
+    for url in child_urls:
+        source_data = requests.get(url)
+        soup = BeautifulSoup(source_data.content, 'html.parser')
+        
+
+        data_categories = soup.find_all("dt", class_=None)
+        non_categories = ['Live Auctions', 'Buying', 'Selling', 'Delivery', 'Past Auctions']
+       
+        whisky_title = soup.find('h1', class_='page-title')
+
+        categories = ['Title']
+        for category in data_categories:
+            if str(category.text) in str(non_categories):
+                pass
+            else:
+                categories.append(category.text)
+
+        values = [(whisky_title.text).strip()]
+        data_values = soup.find_all("dd", class_=None)
+        for value in data_values:
+            values.append(value.text)
+
+        whisky_data = {}
+
+        for category in categories:
+            for value in values:
+                whisky_data[category] = value
+                values.remove(value)
+                break
+
+        print(whisky_data)
+        dict_list.append(whisky_data)
+        sleep(3)
+    return dict_list
--- python_scraper/main.py
+++ python_scraper/main.py
@@ -1,5 +1,11 @@
-from get_urls import get_urls_function
+from get_urls import get_child_url, get_all_data
 
 url = "https://www.whiskyshop.com/auctions/ended?product_list_order=price_desc&auction_ended=August+2019"
 
-get_urls_function(url)
+child_url = "https://www.whiskyshop.com/auctions/a11265-macallan-1995-13-year-old-single-cask-14016-easter-elchies-2009"
+
+
+
+# print(get_child_url(url))
+
+print(get_all_data(get_child_url(url)))
