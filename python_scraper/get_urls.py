fe3f806e7277a5d7b322d97b0169fce9e4bdbdfa
davisansons <davis.ansons@outlook.com>
2022-07-13 17:44:23 +0100

    changed category for loop to generator expression

--- python_scraper/get_urls.py
+++ python_scraper/get_urls.py
@@ -3,35 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
-def get_child_url(url):
-    
-    child_urls = []
 
-    source_data = requests.get(url)
-    soup = BeautifulSoup(source_data.text, 'html.parser')
-    content = soup.find(id='maincontent')
-    content_elements = content.find_all('a', class_='product')
-    for element in content_elements:
-        child_urls.append(element['href'])
-    return child_urls
+def get_child_url(url):
+    src_data = requests.get(url)
+    soup = BeautifulSoup(src_data.text, 'html.parser')
+    content_elements = soup.find(id='maincontent').find_all('a', class_='product')
+    return [x['href'] for x in content_elements]
+
 
 def get_whisky_prices(url):
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render(sleep=1, keep_page=True, scrolldown=1)
-    price_html = r.html.find('.price')
-    price_list = []
-    for price in price_html:
-        price_list.append(price.text)
-    del price_list[1::2]
-    session.close()
-    return price_list
+    with HTMLSession() as session:
+        r = session.get(url)
+        r.html.render(sleep=1, keep_page=True, scrolldown=1)
+        price_html = r.html.find('.price')
+        price_list = [x.text for x in price_html]
+    return price_list[0::2]
 
 
 def get_all_data(child_urls, price_list):
-    i = 0
     dict_list = []
-    for url in child_urls:
+    for i, url in enumerate(child_urls[:2]):
         source_data = requests.get(url)
         soup = BeautifulSoup(source_data.content, 'html.parser')
 
@@ -41,12 +32,8 @@ def get_all_data(child_urls, price_list):
         whisky_title = soup.find('h1', class_='page-title')
         whisky_price_list = price_list
 
-        categories = ['Title', 'Price']
-        for category in data_categories:
-            if str(category.text) in str(non_categories):
-                pass
-            else:
-                categories.append(category.text)
+        categories = [category.text for category in data_categories if str(category.text) not in str(non_categories)]
+        all_categories = ['Title', 'Price'] + categories
 
         values = [(whisky_title.text).strip(), whisky_price_list[i]]
         data_values = soup.find_all("dd", class_=None)
@@ -55,7 +42,7 @@ def get_all_data(child_urls, price_list):
 
         whisky_data = {}
 
-        for category in categories:
+        for category in all_categories:
             for value in values:
                 whisky_data[category] = value
                 values.remove(value)
@@ -63,6 +50,5 @@ def get_all_data(child_urls, price_list):
 
         print(whisky_data)
         dict_list.append(whisky_data)
-        i += 1
         sleep(3)
     return dict_list
--- python_scraper/main.py
+++ python_scraper/main.py
@@ -1,11 +1,14 @@
 from get_urls import get_child_url, get_all_data, get_whisky_prices
 
-url = "https://www.whiskyshop.com/auctions/ended?product_list_order=price_desc&auction_ended=August+2019"
 
-
+def get_url():
+    return "https://www.whiskyshop.com/auctions/ended?product_list_order=price_desc&auction_ended=August+2019"
 
 #get_child_url(url)
 
 #get_whisky_prices(url)
 
-get_all_data(get_child_url(url), get_whisky_prices(url))
+
+if __name__ == "__main__":
+    url = get_url()
+    get_all_data(get_child_url(url), get_whisky_prices(url))
